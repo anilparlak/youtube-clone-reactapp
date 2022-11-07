6f9eb1b9d37a79cf2a9445171a4b8e68312d349a
Anıl Parlak <anilparlak@gmail.com>
2022-11-07 23:26:44 +0300

    set baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjE1MTMwNTYzNDg1YjExZTMyZmJmNyIsImlhdCI6MTY2Nzg1MDAyNH0.o6Xt05qjGXidxxs5LQPAOVhwiXDON9M4Y1UOryInZ8Y";
-const BASE_URL = "http://localhost:5050/api";
+const BASE_URL = "https://youtube-clone-nodejs.herokuapp.com/api";
 
 export function api(){
     return axios.create({
